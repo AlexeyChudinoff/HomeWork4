91a0ed93436cca997b9b0455682d8b1643f31e32
AlexeyChudinoff <201900@mail.ru>
2024-09-17 22:57:17 +0700

    homeWork 4 - 6

--- src/Main.java
+++ src/Main.java
@@ -22,12 +22,59 @@ public class Main {
 
     int speed = 70;
     if (speed <= 60) {
-      System.out.println("скорость : " + speed + " превышения нет, можно ехать спокойно");
+      System.out.println("скорость : " + speed + " - превышения нет, можно ехать спокойно");
     }
     if (speed > 60) {
-      System.out.println("скорость : " + speed + " превышение скорости, придется заплатить штраф");
+      System.out.println(
+          "скорость : " + speed + " - это превышение скорости, придется заплатить штраф");
     }
 
+    int ageBoyn = 50;
+    if (ageBoyn >= 2 && ageBoyn <= 6) {
+      System.out.println(
+          "если возраст человека равен : " + ageBoyn + " лет, то ему нужно ходить в сад");
+    }
+    if (ageBoyn >= 7 && ageBoyn <= 17) {
+      System.out.println(
+          "если возраст человека : " + ageBoyn + " лет, то ему нужно ходить в школу");
+    }
+    if (ageBoyn >= 18 && ageBoyn <= 24) {
+      System.out.println(
+          "если возраст человека : " + ageBoyn + " лет, то ему нужно ходить в институт");
+    }
+    if (ageBoyn > 24) {
+      System.out.println(
+          "если возраст человека : " + ageBoyn + " лет, то ему ходить на работу");
+    }
+
+    int ageChild = 10;
+    if (ageChild < 5) {
+      System.out.println(
+          "если возраст ребенка: " + ageChild + " то ему нельзя кататься на аттракционе");
+    }
+    if (ageChild >= 5 && ageChild <= 14) {
+      System.out.println("если возраст ребенка: " + ageChild
+          + " то ему можно кататься на аттракционе в сопровождении взрослого");
+    }
+    if (ageChild > 14) {
+      System.out.println("если возраст ребенка: " + ageChild
+          + " то ему можно кататься на аттракционе без сопровождения взрослого");
+    }
+
+    int vsegoMest = 102;
+    int sidychihMest = 60;
+    int stoychihMest = vsegoMest - sidychihMest;
+    int tiketsSold = 80; // без этой переменной нет логики в задаче
+    if (tiketsSold <= sidychihMest) {
+      System.out.println(
+          "в вагоне свободно: " + (sidychihMest - tiketsSold) + "сидячих мест и : " + stoychihMest
+              + "стоячих мест");
+    }
+    if (tiketsSold > sidychihMest && tiketsSold <= vsegoMest) {
+      System.out.println(
+          "в вагоне нет свободных сидячих мест, осталось только: " + (102 - tiketsSold)
+              + " стоячих места");
+    }
 
   }
 }
